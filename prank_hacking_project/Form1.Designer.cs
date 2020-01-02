f40aedb15e6360cac5875391dc200874475aaf1f
Jan Erik <jan.erik.zwilling@gmail.com>
2020-01-02 16:26:01 +0100

    lable hinzugefügt

--- prank_hacking_project/Form1.Designer.cs
+++ prank_hacking_project/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -39,22 +40,35 @@
             this.progressBar1.TabIndex = 0;
             this.progressBar1.Value = 50;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(489, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Retrieving Data";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Name = "Form1";
             this.Text = "Prank Hacking Fenster";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
